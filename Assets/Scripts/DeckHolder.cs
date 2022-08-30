5a77d33679f8925d9b7c4f033c66363dc1274afd
Artyom <90120253+Re1aine@users.noreply.github.com>
2022-08-31 00:06:51 +0300

    Update deckholder

--- Assets/Scripts/DeckHolder.cs
+++ Assets/Scripts/DeckHolder.cs
@@ -1,6 +1,7 @@
 ﻿
 using System.Collections.Generic;
 using Infrastructure.StaticData;
+using Infrastructure.StaticData.Cards;
 using UnityEngine;
 
 public class DeckHolder : IDeckHolder
@@ -35,12 +36,14 @@ public class DeckHolder : IDeckHolder
     public void PushCard(CardId cardId)
     {
         _discardPile.Enqueue(cardId);
+        Debug.Log($"{cardId} push to discardPile, discardPile.Count = {_discardPile.Count}");
     }
 
     private void Refresh()
     {
         _drawPile = _discardPile;
         _discardPile = new Queue<CardId>();
+        Debug.Log($"drawPile.Count = {_drawPile.Count}, discardPile.Count = {_discardPile.Count}");
     }
 
     private bool IsDrawPileEmpty() => _drawPile.Count == 0;
--- Assets/Scripts/IDeckHolder.cs
+++ Assets/Scripts/IDeckHolder.cs
@@ -3,6 +3,7 @@ using Card;
 using Infrastructure;
 using Infrastructure.Factories;
 using Infrastructure.StaticData;
+using Infrastructure.StaticData.Cards;
 using UnityEngine;
 
 public interface IDeckHolder
