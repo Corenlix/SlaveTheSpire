8bf8082aa4f3b80b8d40c6b3b1c77348e75e85a2
Corenlix <corenlix@gmail.com>
2022-09-02 21:54:18 +0300

    Add attack processor test

---
+++ Assets/Editor/AttackProcessorTest.cs
@@ -0,0 +1,27 @@
+﻿using Entities;
+using Entities.Enemies;
+using FluentAssertions;
+using Infrastructure.StaticData.Enemies;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Editor
+{
+    public class AttackProcessorTest
+    {
+        [Test]
+        public void Test()
+        {
+            //Arrange
+            Enemy target = new GameObject().AddComponent<TestEnemy>();
+            var targetStaticData = Substitute.For<TestEnemyStaticData>();
+            targetStaticData.MaxHealth.Returns(20);
+            target.Init(targetStaticData);
+
+            //Act
+
+            //Assert
+            target.EntityHealth.Health.Should().Be(20);
+        }
+    }
+}
